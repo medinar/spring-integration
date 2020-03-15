6ae1eefc061a37b140c3bb646b5d1f26a5445708
Mhel Medina <rommel.d.medina@gmail.com>
2020-03-15 08:04:17 +0800

    Initial commit

---
+++ src/main/java/com/medinar/spring/integration/Application.java
@@ -0,0 +1,12 @@
+package com.medinar.spring.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
